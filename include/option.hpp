3c3eedc1bb083db2280ee3621e0839658fffaf00
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2023-11-01 20:54:21 +0500

    Add base `opt::option` methods and operators

--- include/option.hpp
+++ include/option.hpp
@@ -1,16 +1,204 @@
 #pragma once
 
+#include <type_traits>
+#include <utility>
+#include <cstdint>
+#include <memory>
+#include <new>
+
 namespace opt {
 
-struct none_t { constexpr explicit none_t(int) {} };
-inline constexpr none_t none{};
+namespace impl {
+    struct none_tag_ctor {};
+}
 
-template<class T>
-class option {
+struct none_t {
+    constexpr explicit none_t(impl::none_tag_ctor) {}
+};
+inline constexpr none_t none{impl::none_tag_ctor{}};
+
+namespace impl {
+    enum class no_sentinel_t {};
+
+    template<class T>
+    struct sentinel_maker {
+        static constexpr no_sentinel_t value{};
+    };
+    template<> struct sentinel_maker<bool> {
+        static constexpr std::uint_least8_t value = 2;
+    };
+
+}
+
+namespace impl {
+    template<class T>
+    using remove_cvref = std::remove_cv_t<std::remove_reference_t<T>>;
+
+    struct nontrivial_dummy_t {
+        constexpr nontrivial_dummy_t() noexcept {}
+    };
+
+    template<class T, auto sentinel,
+        bool store_state = std::is_same_v<decltype(sentinel), no_sentinel_t>,
+        bool is_trivially_destructible = std::is_trivially_destructible_v<T>>
+    struct option_base;
+
+    template<class T, auto sentinel>
+    struct option_base<T, sentinel, /*store_state=*/true, /*is_trivially_destructible=*/true> {
+        constexpr option_base() noexcept : dummy(), has_value(false) {}
+
+        template<class... Args>
+        constexpr option_base(Args&&... args)
+            noexcept(std::is_nothrow_constructible_v<T, Args...>)
+            : value(std::forward<Args>(args)...), has_value(true) {}
+
+        constexpr void reset() noexcept {
+            has_value = false;
+        }
+
+        union {
+            nontrivial_dummy_t dummy;
+            T value;
+        };
+        bool has_value;
+    };
+    template<class T, auto sentinel>
+    struct option_base<T, sentinel, /*store_state=*/true, /*is_trivially_destructible=*/false> {
+        constexpr option_base() noexcept : dummy(), has_value(false) {}
+
+        template<class... Args>
+        constexpr option_base(Args&&... args)
+            : value(std::forward<Args>(args)...), has_value(true) {}
+
+        ~option_base() {
+            if (has_value) {
+                value.~T();
+            }
+        }
+
+        constexpr option_base(const option_base&) = default;
+        constexpr option_base(option_base&&) = default;
+        constexpr option_base& operator=(const option_base&) = default;
+        constexpr option_base& operator=(option_base&&) = default;
+        
+        constexpr void reset() noexcept(std::is_nothrow_destructible_v<T>) {
+            if (has_value) {
+                value.~T();
+                has_value = false;
+            }
+        }
+
+        union {
+            nontrivial_dummy_t dummy;
+            T value;
+        };
+        bool has_value;
+    };
+
+    template<class T, class... Args>
+    constexpr void construct_at(T& obj, Args&&... args) {
+        ::new(static_cast<void*>(std::addressof(obj))) T(std::forward<Args>(args)...);
+    }
+}
+
+template<class T, auto sentinel = impl::sentinel_maker<T>::value>
+class option : private impl::option_base<T, sentinel> {
+    using base = impl::option_base<T, sentinel>;
 public:
 
-private:
+    constexpr option() noexcept : base() {}
+    constexpr option(opt::none_t) noexcept : base() {}
 
+    constexpr option(const option& other) noexcept(std::is_nothrow_constructible_v<T, const T&>) {
+        if (other) {
+            impl::construct_at(base::value, *other);
+        }
+    }
+    constexpr option(option&& other) noexcept(std::is_nothrow_constructible_v<T, T&&>) {
+        if (other) {
+            impl::construct_at(base::value, std::move(*other));
+        }
+    }
+
+    constexpr option& operator=(opt::none_t) noexcept {
+        reset();
+        return *this;
+    }
+    constexpr option& operator=(const option& other) noexcept(std::is_nothrow_constructible_v<T, const T&> && std::is_nothrow_assignable_v<T&, const T&>) {
+        if (other) {
+            _assign(*other);
+        } else {
+            reset();
+        }
+        return *this;
+    }
+    constexpr option& operator=(option&& other) noexcept(std::is_nothrow_constructible_v<T, T> && std::is_nothrow_assignable_v<T&, T>) {
+        if (other) {
+            _assign(std::move(*other));
+        } else {
+            reset();
+        }
+        return *this;
+    }
+    template<class U = T, std::enable_if_t<
+        !std::is_same_v<option, impl::remove_cvref<U>>
+        && (!std::is_scalar_v<T> || !std::is_same_v<T, std::decay_t<U>>)
+        && std::is_constructible_v<T, U> && std::is_assignable_v<T&, U>
+    , int> = 0>
+    constexpr option& operator=(U&& value) noexcept(std::is_nothrow_assignable_v<T&, U> && std::is_nothrow_constructible_v<T, U>) {
+        _assign(std::forward<U>(value));
+        return *this;
+    }
+
+    template<class First, class... Args, std::enable_if_t<std::is_constructible_v<T, First, Args...>, int> = 0>
+    constexpr option(First&& first, Args&&... args) noexcept(std::is_nothrow_constructible_v<T, First, Args...>)
+        : base(std::forward<First>(first), std::forward<Args>(args)...) {}
+
+    constexpr void reset() noexcept {
+        base::reset();
+    }
+
+    template<class... Args>
+    constexpr T& emplace(Args&&... args) {
+        reset();
+        impl::construct_at(base::value, std::forward<Args>(args)...);
+        base::has_value = true;
+        return *(*this);
+    }
+
+    constexpr bool has_value() const noexcept {
+        return base::has_value;
+    }
+    constexpr explicit operator bool() const noexcept { return has_value(); }
+
+    constexpr const T* operator->() const noexcept {
+        return std::launder(&base::value);
+    }
+    constexpr T* operator->() noexcept {
+        return std::launder(&this->base::value);
+    }
+    constexpr const T& operator*() const& noexcept {
+        return *std::launder(&this->base::value);
+    }
+    constexpr T& operator*() & noexcept {
+        return *std::launder(&this->base::value);
+    }
+    constexpr const T&& operator*() const&& noexcept {
+        return std::move(*std::launder(&this->base::value));
+    }
+    constexpr T&& operator*() && noexcept {
+        return std::move(*std::launder(&this->base::value));
+    }
+private:
+    template<class U>
+    constexpr void _assign(U&& other) {
+        if (has_value()) {
+            this->base::value = std::forward<U>(other);
+        } else {
+            emplace(std::forward<U>(other));
+        }
+        this->base::has_value = true;
+    }
 };
 
 }
--- test/option.test.cpp
+++ test/option.test.cpp
@@ -4,8 +4,71 @@
 
 namespace {
 
-TEST(a, b) {
-    EXPECT_EQ(1, 1);
+// https://stackoverflow.com/a/67059296
+template<class T>
+T& unmove(T&& x) { return static_cast<T&>(x); }
+
+static_assert(std::is_trivially_destructible_v<opt::option<int>>);
+
+struct trivial_struct {};
+static_assert(std::is_trivially_destructible_v<opt::option<trivial_struct>>);
+
+struct nontrivial_struct {
+    ~nontrivial_struct() {}
+};
+static_assert(!std::is_trivially_destructible_v<opt::option<nontrivial_struct>>);
+
+struct option : ::testing::Test {
+
+};
+
+TEST_F(option, default_ctor) {
+    opt::option<int> a;
+    EXPECT_FALSE(a.has_value());
+
+    opt::option<int> b(opt::none);
+    EXPECT_FALSE(b.has_value());
+
+    opt::option<int> c = opt::none;
+    EXPECT_FALSE(c.has_value());
+}
+TEST_F(option, assigment) {
+    opt::option<int> a = 1;
+    a = opt::none;
+    EXPECT_FALSE(a.has_value());
+    a = unmove(opt::option<int>(2));
+    EXPECT_TRUE(a.has_value());
+    EXPECT_EQ(*a, 2);
+    a = unmove(opt::option<int>(opt::none));
+    EXPECT_FALSE(a.has_value());
+    a = opt::option<int>(3);
+    EXPECT_TRUE(a.has_value());
+    EXPECT_EQ(*a, 3);
+    a = opt::option<int>(opt::none);
+    EXPECT_FALSE(a.has_value());
+    a = unmove(4);
+    EXPECT_TRUE(a.has_value());
+    EXPECT_EQ(*a, 4);
+    a = 5;
+    EXPECT_TRUE(a.has_value());
+    EXPECT_EQ(*a, 5);
+}
+
+TEST_F(option, reset) {
+    opt::option<int> a;
+    EXPECT_FALSE(a.has_value());
+    a.reset();
+    EXPECT_FALSE(a.has_value());
+    a = 1;
+    EXPECT_TRUE(a.has_value());
+    a.reset();
+    EXPECT_FALSE(a.has_value());
+}
+TEST_F(option, emplace) {
+    opt::option<int> a;
+    a.emplace(1);
+    EXPECT_TRUE(a.has_value());
+    EXPECT_EQ(*a, 1);
 }
 
 }
