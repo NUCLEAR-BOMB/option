9e79556813667bed095cd6a14e7ac4e8529a7f6b
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2023-11-03 18:30:25 +0500

    Remove python check in 'clang_tidy.cmake'

--- cmake/clang_tidy.cmake
+++ cmake/clang_tidy.cmake
@@ -16,11 +16,11 @@ function(add_clang_tidy_target name)
         message(WARNING "[clang-tidy] Error with code '${status_code}' occurred during downloading 'run-clang-tidy.py': ${error_msg}")
         return()
     endif()
-    find_package(Python 3 QUIET)
-    if (NOT Python_FOUND)
-        message(WARNING "[clang-tidy] Python3 not found")
-        return()
-    endif()
+    #find_package(Python 3 QUIET)
+    #if (NOT Python_FOUND)
+    #    message(WARNING "[clang-tidy] Python3 not found")
+    #    return()
+    #endif()
     find_program(clang_tidy_binary clang-tidy)
     if (clang_tidy_binary STREQUAL "clang_tidy_binary-NOTFOUND")
         message(WARNING "[clang-tidy] clang-tidy not found")
@@ -29,7 +29,7 @@ function(add_clang_tidy_target name)
 
     set(clang_tidy_config "${PROJECT_SOURCE_DIR}/.clang-tidy")
     add_custom_target(${name}
-        COMMAND "${Python_EXECUTABLE}" "${run_clang_tidy}" -quiet -p "${PROJECT_BINARY_DIR}" -use-color
+        COMMAND "python" "${run_clang_tidy}" -quiet -p "${PROJECT_BINARY_DIR}" -use-color
         -clang-tidy-binary "${clang_tidy_binary}" -config-file "${clang_tidy_config}"
         WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}" VERBATIM
         DEPENDS "${run_clang_tidy}"
