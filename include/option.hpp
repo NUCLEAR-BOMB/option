68fe1ca59f2c644e171f3cbe7ed0e24508c9eafc
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2023-11-24 23:28:41 +0500

    Fix typo

--- include/option.hpp
+++ include/option.hpp
@@ -151,12 +151,12 @@ namespace impl {
     template<class To, class From>
     constexpr void bit_copy(To& to, const From& from) noexcept {
         static_assert(sizeof(To) == sizeof(From));
-#ifdef defined(__GNUC__) || defined(__GNUG__)
+#if defined(__GNUC__) || defined(__GNUG__)
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Werror=class-memaccess"
 #endif
         std::memcpy(&to, &from, sizeof(To));
-#ifdef defined(__GNUC__) || defined(__GNUG__)
+#if defined(__GNUC__) || defined(__GNUG__)
     #pragma GCC diagnostic pop
 #endif
     }
