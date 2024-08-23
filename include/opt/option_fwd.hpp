20c2e555f620fb0c357dbc37f8b419cc6c838a93
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-08-23 18:03:27 +0500

    Add licence and licence headers

--- include/opt/option_fwd.hpp
+++ include/opt/option_fwd.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+// Copyright 2024.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
 namespace opt {
 
 template<class T>
--- test/utils.hpp
+++ test/utils.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+// Copyright 2024.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+
 #include <iostream>
 #include <opt/option.hpp>
 #include <doctest/doctest.h>
