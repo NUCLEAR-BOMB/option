77a9a1fb2c64c5195e62e0e833dc4f4c1fa9e6fc
NUCLEAR-BOMB <61628949+NUCLEAR-BOMB@users.noreply.github.com>
2024-11-15 11:31:01 +0500

    Remove `option_fwd.hpp` inline code from `option.hpp`

--- include/opt/option.hpp
+++ include/opt/option.hpp
@@ -13,35 +13,7 @@
 #include <cstdint>
 #include <initializer_list>
 
-#if __has_include(<opt/option_fwd.hpp>)
-    #include <opt/option_fwd.hpp>
-#else
-namespace opt {
-template<class T>
-class option;
-
-template<class T, class = void>
-struct option_traits;
-
-template<class>
-struct is_option { static constexpr bool value = false; };
-template<class T>
-struct is_option<option<T>> { static constexpr bool value = true; };
-
-template<class T>
-inline constexpr bool is_option_v = is_option<T>::value;
-
-struct option_tag {};
-
-namespace impl {
-    struct none_tag {};
-}
-struct none_t {
-    constexpr explicit none_t(impl::none_tag) {}
-};
-inline constexpr none_t none{impl::none_tag{}};
-}
-#endif
+#include <opt/option_fwd.hpp>
 
 #ifdef __INTEL_COMPILER
     #define OPTION_CLANG 0
